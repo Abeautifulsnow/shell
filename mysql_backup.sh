2a8befdc22717c34d577888b160462b0338ca3f9
Abeautifulsnow <1491444340@qq.com>
2019-12-22 23:57:44 +0800

    添加新sh

---
+++ mysql_backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+date_now=$(date "+%Y%m%d-%H%M%S")
+backUpFolder=/home/db/backup/mysql
+username="root"
+password="ln122920"
+db_name="work_blog"
+# 定义备份文件名
+fileName="${db_name}_${date_now}.sql"
+# 定义备份文件目录
+backUpFileName="${backUpFolder}/${fileName}"
+echo "starting backup mysql ${db_name} at ${date_now}."
+/usr/bin/mysqldump -u${username} -p${password} --lock-all-tables --flush-logs ${db_name} > "${backUpFileName}"
+# 进入到备份文件夹目录
+cd ${backUpFolder}
+# 压缩备份文件
+tar zcvf "${fileName}".tar.gz "${fileName}"
+
+date_end=$(date "+%Y%m%d-%H%M%S")
+echo "finish backup mysql database ${db_name} at ${date_end}."
